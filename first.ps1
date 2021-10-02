fcb5b477876467f9c6d744e5ad5533e52a0d330c
Panuganti, Lakshmanachari <lpanuganti@deloitte.com>
2021-10-02 14:59:11 +0530

    This is my second comit

--- first.ps1
+++ first.ps1
@@ -1,1 +1,2 @@
 Write-Host "This is my first PowerShell program in Github"
+Write-Host 'vscode://vscode.github-authentication/did-authenticate?windowid=1&code=5ba8823e6dc36a93f63a&state=9cbc6a1f-1461-4bae-81ad-a6c7f757313f'
