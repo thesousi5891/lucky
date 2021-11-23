1de503c761fb91c0878d88805be182810e7dfadc
Panuganti, Lakshmanachari <lpanuganti@deloitte.com>
2021-11-23 17:38:47 +0530

    cl added

--- first.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "This is my first PowerShell program in Github"
-Write-Host 'vscode://vscode.github-authentication/did-authenticate?windowid=1&code=5ba8823e6dc36a93f63a&state=9cbc6a1f-1461-4bae-81ad-a6c7f757313f'
-Write-Host "Testing the first branch in Github"
